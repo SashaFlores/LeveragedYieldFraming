28296dd42a100b3f5d0e94831d6517f93413705e
SashaFlores <sashaflores207@gmail.com>
2021-09-18 22:37:55 -0400

    remote execution

---
+++ README.md
@@ -0,0 +1,24 @@
+# Flash-Loan
+
+Leverage Yield Farming Smart Contracts based on Compound and dydx flashloans.
+
+## Tools & Systems:
+
+* [npm](https://www.npmjs.com/) based dap.
+* [Truffle framework](https://www.trufflesuite.com/) for Smart Contracts.
+* [Ganache-cli](https://www.trufflesuite.com/) for forking Ethereum mainnet.
+* [MetaMask](https://metamask.io/) Wallet.
+* [Infura](https://infura.io/) API & Project ID.
+* [Open Zeppelin](https://github.com/OpenZeppelin/openzeppelin-contracts) library.
+* [Money Legos](https://money-legos.studydefi.com/#/compound) library that provides you with the mainnet addresses, ABIs, and Interfaces for DeFi protocols.
+
+## Clone and run packages:
+
+    git clone git@github.com:SashaFlores/Flash-Loans.git
+    cd Flash-Loan
+    npm install
+
+## Successful execution Shows compound token earnings every 10s
+
+
+![Screenshots/earning.jpg](Screenshots/earning.jpg)# flash-loan
---
+++ truffle-config.js
@@ -0,0 +1,48 @@
+
+
+// const HDWalletProvider = require('@truffle/hdwallet-provider');
+// const infuraKey = "fj4jll3k.....";
+//
+// const fs = require('fs');
+// const mnemonic = fs.readFileSync(".secret").toString().trim();
+
+module.exports = {
+  networks: {
+    development: {
+     host: "127.0.0.1",     // Localhost (default: none)
+     port: 8545,            // Standard Ethereum port (default: none)
+     network_id: "*",       // Any network (default: none)
+    },
+  },
+
+  // Set default mocha options here, use special reporters etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.5.7",    // Fetch exact version from solc-bin (default: truffle's version)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+       optimizer: {
+         enabled: true,
+         runs: 200
+       },
+       evmVersion: "byzantium"
+      }
+    }
+  },
+  contracts_directory: "./src/contracts/",
+  contracts_build_directory: "./src/abis/",
+
+  // Truffle DB is currently disabled by default; to enable it, change enabled: false to enabled: true
+  //
+  // Note: if you migrated your contracts prior to enabling this field in your Truffle project and want
+  // those previously migrated contracts available in the .db directory, you will need to run the following:
+  // $ truffle migrate --reset --compile-all
+
+  db: {
+    enabled: false
+  }
+};
